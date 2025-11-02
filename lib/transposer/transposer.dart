06ef115b9f942077daabe37ad1dc1800fbe1ed80
Szymon <szymon@MacBook-Pro-Szymon.local>
2025-11-02 22:56:17 +0100

    Updates in transposer

--- lib/transposer/transposer.dart
+++ lib/transposer/transposer.dart
@@ -25,6 +25,37 @@ List<String> generateMajorScale(String root) {
   return scale;
 }
 
+String transposeProgression(String progression, String fromKey, String toKey) {
+  int fromIndex = majorKeys.contains(fromKey) ? majorKeys.indexOf(fromKey) : majorKeysFlat.indexOf(fromKey);
+  int toIndex = majorKeys.contains(toKey) ? majorKeys.indexOf(toKey) : majorKeysFlat.indexOf(toKey);
+  
+  if(fromIndex == -1 || toIndex == -1) {
+    throw ArgumentError('Invalid fromKey or toKey');
+  }
+
+  bool useFlats = majorKeysFlat.contains(toKey);
+  int semitoneDifference = (toIndex - fromIndex);
+  String transposedProgression = '';
+
+  for(int i = 0; i < progression.length; i++) {
+    String chord = progression[i];
+    int currentCordIndex = useFlats ? majorKeysFlat.indexOf(chord) : majorKeys.indexOf(chord);
+    print('Processing chord: $chord');
+
+    if(chord == ' ' || chord.isEmpty || chord == '|' || chord == '/' || currentCordIndex == -1) {
+      transposedProgression += chord;
+      continue;
+    }
+    
+    String transposedChord = useFlats ? majorKeysFlat[(majorKeysFlat.indexOf(chord) + semitoneDifference) % 12] : majorKeys[(majorKeys.indexOf(chord) + semitoneDifference) % 12];
+    transposedProgression += transposedChord;
+  }
+  return transposedProgression;
+}
 void main() {
-  print(generateMajorScale("Bb"));  // Example usage
+  print(generateMajorScale("Bb")); 
+  String progression = '|Dmaj7 / / Bm|A|';
+  String transposedPrograssion = transposeProgression(progression, 'D', 'Bb'); 
+  print(progression);
+  print(transposedPrograssion);// Example usage
 }
